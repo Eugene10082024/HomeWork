628c14e575fd474fd402cf0adcfe7cf107f0272e
Aleksey-Sarafanov <81502379+Aleksey-10081967@users.noreply.github.com>
2021-06-24 09:36:29 +0300

    Update logs-nginx.sh

--- HW-lesson-09/dop_files/logs-nginx.sh
+++ HW-lesson-09/dop_files/logs-nginx.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
+
 #Проверка pid файла и создание его при запуске
 #
-
 if [[ -f /var/run/an-log.pid ]] ; then
-    echo "Скрипт уже выполняется. Аварийное завершение !!!"
-    exit 1
+        echo "Скрипт уже выполняется. Аварийное завершение !!!"
+        exit 1
 else
-    echo $$ > /var/run/an-log.pid
+        echo $$ > /var/run/an-log.pid
 fi
 
-# Задание переменных файлов
 file_log=access.log
 file_rez=rezult.log
 file_numline=numline
+sendaddress="sarafanov-67@rambler.ru"
+
 
 # Проверка на наличие файла log используемого для анализа
 
-[[ if ! -f $file_log ]]; then
+if [[ ! -f $file_log ]]; then
         echo 'Файл для проведения анализа отсутствует !!!!' > $file_rez
         echo 'Ожидался - ' $file_log >> $file_rez
+        mail -s "Результаты анализа log файла nginx" "$sendaddress" < $file_rez
         rm -rf /var/run/an-log.pid
         exit 1
 fi
+
+
 
 # Проверка на наличие файла с номером строки для продолжения подсчета
 if [[ ! -f $file_numline ]]; then
@@ -29,7 +33,9 @@ if [[ ! -f $file_numline ]]; then
         echo 0 > $file_numline
 fi
 
+
 # Удаление пустных строк из файла log
+
 cp $file_log tmp.log; rm -rf $file_log
 > $file_log
 awk '!/^$/' tmp.log >> $file_log
@@ -41,8 +47,13 @@ num_line=$(cat $file_numline 2>/dev/null);status=$?
 # Сколько строк в файле
 checkLines=$(wc $file_log | awk '{print $1}')
 
-# Определяем дату и время начала и окончания расчета статистики
+
+# Если возвращается пустое значение, т.е. его нет, тогда считаем количество строк и записываем значение в файл
+    # Определяем дату и время начала и окончания расчета статистики
+
+
 # Дата начала и конца
+# timebegin=$(awk '{print $4}' $file_log  | sed -n "$(($number+1))"p | cut -c2- )
 timebegin=$( awk "NR>$(($num_line+1))" $file_log | awk '{print $4}' | sed -n "$(($number+1))"p | cut -c2- )
 timeend=$(tail -n 1 $file_log | awk '{print $4 }' | cut -c2- )
 
@@ -50,11 +61,11 @@ echo 'Период проведения анализа. Начало: '$timebegi
 
 # Определение количества IP запросов с IP адресов
 echo '10 IP адресов с макс. коичеством обращений за указанный период:' >> $file_rez
-awk "NR>$(($num_line+1))" $file_log | awk '{print $1}' | sort | uniq -c | sort -rn | head -n 10 >> $file_rez
+awk "NR>$(($num_line+1))"  $file_log |  awk '{print $1}' | sort | uniq -c | sort -rn | head -n 10  >> $file_rez
 
 # Y количества адресов
 echo '5 http адресов с максимальным количеством обращений за указанный период:' >> $file_rez
-awk "NR>$(($num_line+1))" $file_log | awk '($9 ~ /200/)' | awk '{print $7}' | sort | uniq -c | sort -rn | head -n 5 >> $file_rez
+awk "NR>$(($num_line+1))"  $file_log | awk '($9 ~ /200/)' | awk '{print $7}' | sort | uniq -c | sort -rn | head -n 5 >> $file_rez
 
 # Количество ответов с кодом 200
 echo 'Количество ответов с кодом 200' >> $file_rez
@@ -66,11 +77,10 @@ awk "NR>$(($num_line+1))"  $file_log | awk '($9 ~ /3/)' |cut -d '"' -f3 | cut -d
 awk "NR>$(($num_line+1))"  $file_log | awk '($9 ~ /4/)' |cut -d '"' -f3 | cut -d ' ' -f2 | sort | uniq -c | sort -rn >> $file_rez
 awk "NR>$(($num_line+1))"  $file_log | awk '($9 ~ /5/)' |cut -d '"' -f3 | cut -d ' ' -f2 | sort | uniq -c | sort -rn >> $file_rez
 
+
 # Запись количества строк в файле
 echo $checkLines > $file_numline
-
 # Отправка почты
-cat $file_rez | mail -s "Анализ access.log за последний час" sarafanov67@rambler.ru
+mail -s "Результаты анализа log файла nginx" "$sendaddress" < $file_rez
 
-# Удаление an-log.pid
 rm -rf /var/run/an-log.pid
